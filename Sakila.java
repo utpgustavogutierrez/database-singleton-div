98437183827098e1a0ee57e9ed68ee73ad2259bd
sicalo330 <noah.esteban9@gmail.com>
2024-04-06 00:15:41 -0500

    Singletonizar statement

--- Sakila.java
+++ Sakila.java
@@ -4,30 +4,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Sakila
-{
-  public static void main(String[] args)
-  {
-    // NOTE: Connection and Statement are AutoClosable.
-    //       Don't forget to close them both in order to avoid leaks.
-    try
-    (
-      // create a database connection
-      Connection connection = DriverManager.getConnection("jdbc:sqlite:./sqlite-sakila.db");
-      Statement statement = connection.createStatement();
-    )
-    {
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+public class Sakila {
+  public static void main(String[] args) {
+    try {
+      Singleton singleton = Singleton.getInstance();
+      Singleton singleton2 = Singleton.getInstance();
+      // Se evidencian que las direcciones de memoria son las mismas por lo tanto
+      // singleton funciona
+      System.out.println(singleton);
+      System.out.println(singleton2);
+      try (Statement statement = singleton.getStatement()) {
+        statement.setQueryTimeout(30); // set timeout to 30 sec.
 
-      ResultSet rs = statement.executeQuery("select * from film");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("title = " + rs.getString("title"));
+        ResultSet rs = statement.executeQuery("select * from film");
+        while (rs.next()) {
+          // read the result set
+          System.out.println("title = " + rs.getString("title"));
+        }
       }
-    }
-    catch(SQLException e)
-    {
+      singleton.close();
+    } catch (SQLException e) {
       // if the error message is "out of memory",
       // it probably means no database file is found
       e.printStackTrace(System.err);
